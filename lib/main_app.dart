e83fc88542b52e24f677d0be51cc746994a5cb38
Kaizonaro <zonaro@outlook.com>
2024-07-02 15:43:04 -0300

    chore: Update search functionality and fix recent searches bug

--- lib/main_app.dart
+++ lib/main_app.dart
@@ -19,99 +19,114 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final PageController controller = PageController();
-    return RestartWidget(
-      child: Sizer(
-        builder: (context, orientation, deviceType) => GetMaterialApp(
-          themeMode: ThemeMode.system,
-          theme: ThemeData.light().copyWith(primaryColor: mainColor),
-          darkTheme: ThemeData.dark().copyWith(primaryColor: mainColor),
-          home: Scaffold(
-            floatingActionButton: GestureDetector(
-              onLongPress: () async {
-                if (categories.isNotEmpty) {
-                  await showModalBottomSheet(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return ListView(
-                        children: [
-                          for (var category in categories)
-                            ListTile(
-                              leading: Icon(categoryIcon(category)),
-                              trailing: Text(filteredAppsByCategory[category]?.length.toString() ?? "0"),
-                              title: Text(category),
-                              onTap: () async {
-                                context.pop();
-                                await showSearch(context: context, delegate: MyAppSearchDelegate(category));
-                              },
-                            ),
-                        ],
-                      );
-                    },
-                  );
-                }
+    return Sizer(
+      builder: (context, orientation, deviceType) => GetMaterialApp(
+        themeMode: ThemeMode.system,
+        theme: ThemeData.light().copyWith(primaryColor: mainColor),
+        darkTheme: ThemeData.dark().copyWith(primaryColor: mainColor),
+        home: const RestartWidget(
+          child: HomePage(),
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final PageController controller = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: GestureDetector(
+        onLongPress: () async {
+          if (categories.isNotEmpty) {
+            await showModalBottomSheet(
+              context: context,
+              builder: (BuildContext context) {
+                return ListView(
+                  children: [
+                    for (var category in categories)
+                      ListTile(
+                        leading: Icon(categoryIcon(category)),
+                        trailing: Text(filteredAppsByCategory[category]?.length.toString() ?? "0"),
+                        title: Text(category),
+                        onTap: () async {
+                          context.pop();
+                          await showSearch(context: context, delegate: MyAppSearchDelegate(category));
+                        },
+                      ),
+                  ],
+                );
               },
-              child: FloatingActionButton(
-                child: const Icon(Icons.search),
-                onPressed: () async {
-                  await showSearch(context: context, delegate: MyAppSearchDelegate());
-                },
+            );
+          }
+        },
+        child: FloatingActionButton(
+          child: const Icon(Icons.search),
+          onPressed: () async {
+            await showSearch(context: context, delegate: MyAppSearchDelegate());
+          },
+        ),
+      ),
+      floatingActionButtonLocation: dockedAppsList.isNotEmpty ? FloatingActionButtonLocation.endContained : null,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const DigitalClock(),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.folder),
+            onPressed: () async {
+              await [
+                Permission.storage,
+                Permission.manageExternalStorage,
+              ].request();
+              if (await Permission.storage.isGranted || await Permission.manageExternalStorage.isGranted) {}
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () async {
+              await context.push(const SettingsScreen());
+              context.restartApp();
+            },
+          ),
+        ],
+      ),
+      bottomNavigationBar: dockedAppsList.isEmpty
+          ? null
+          : BottomAppBar(
+              child: ListView(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                children: [
+                  for (var docked in dockedAppsList) ...[
+                    AppTile(
+                      application: docked as ApplicationWithIcon,
+                      gridColumns: gridColumns.lockMin(3),
+                      showLabel: false,
+                      onPop: () {},
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                  ]
+                ],
               ),
             ),
-            floatingActionButtonLocation: dockedAppsList.isNotEmpty ? FloatingActionButtonLocation.endContained : null,
-            appBar: AppBar(
-              automaticallyImplyLeading: false,
-              title: const DigitalClock(),
-              actions: [
-                IconButton(
-                  icon: const Icon(Icons.folder),
-                  onPressed: () async {
-                    await [
-                      Permission.storage,
-                      Permission.manageExternalStorage,
-                    ].request();
-                    if (await Permission.storage.isGranted || await Permission.manageExternalStorage.isGranted) {}
-                  },
-                ),
-                IconButton(
-                  icon: const Icon(Icons.settings),
-                  onPressed: () async {
-                    await context.push(const SettingsScreen());
-                    context.restartApp();
-                  },
-                ),
-              ],
-            ),
-            bottomNavigationBar: dockedAppsList.isEmpty
-                ? null
-                : BottomAppBar(
-                    child: ListView(
-                      shrinkWrap: true,
-                      scrollDirection: Axis.horizontal,
-                      children: [
-                        for (var docked in dockedAppsList) ...[
-                          AppTile(
-                            application: docked as ApplicationWithIcon,
-                            gridColumns: gridColumns.lockMin(3),
-                            showLabel: false,
-                            onPop: () {},
-                          ),
-                          const SizedBox(
-                            width: 10,
-                          ),
-                        ]
-                      ],
-                    ),
-                  ),
-            body: PageView(
-              controller: controller,
-              children: const [
-                AppPage(),
-                FilePage(),
-              ],
-            ),
-          ),
-        ),
+      body: PageView(
+        controller: controller,
+        children: const [
+          AppPage(),
+          FilePage(),
+        ],
       ),
     );
   }
