f9c4697e23cddcec42d71aaf15c1b600d6ac71db
Kaizonaro <zonaro@outlook.com>
2025-08-01 02:30:15 -0300

    Remove extra blank line in game_dashboard.dart
    Cleaned up the import section by removing an unnecessary blank line at the top of the file.

--- lib/pages/game_dashboard.dart
+++ lib/pages/game_dashboard.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: use_build_context_synchronously
-
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:innerlibs/innerlibs.dart';
